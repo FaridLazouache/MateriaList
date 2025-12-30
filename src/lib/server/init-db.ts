fbd9da4b0adaecfe9d06a7e113879d0fbd5ff5fb
LeChatBear <lazouache.farid@gmail.com>
2025-12-30 17:23:51 +0100

    docs: added function explainations

--- src/lib/server/init-db.ts
+++ src/lib/server/init-db.ts
@@ -1,8 +1,10 @@
-// src/lib/server/init-db.ts
 import { db } from "./db";
 import fs from "fs";
 import path from "path";
 
+/**
+ * Initializes the database schema by executing the SQL statements
+ */
 export function initDb() {
   const schemaPath = path.resolve("src/database/init.sql");
   const schema = fs.readFileSync(schemaPath, "utf-8");
@@ -12,6 +14,9 @@ export function initDb() {
   console.info("✅ SQLite schema initialized");
 }
 
+/**
+ * Seeds the database with test data by executing the SQL statements
+ */
 export function testDB() {
   const schemaPath = path.resolve("src/database/test.sql");
   const schema = fs.readFileSync(schemaPath, "utf-8");
