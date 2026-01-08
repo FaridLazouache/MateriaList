c4b2e6b655e22559baa71798e43cd109171e5e33
LeChatBear <lazouache.farid@gmail.com>
2026-01-08 21:02:03 +0100

    docs: removed "Type Of Change" subsection chore: "utf-8" in a const

--- src/lib/server/init-db.ts
+++ src/lib/server/init-db.ts
@@ -4,10 +4,11 @@ import path from "path";
 
 const __dirname = process.cwd();
 
-// dossier data/db à la racine du projet
 const dataDir = path.resolve(__dirname, `src/conf/database`);
 const initPath = path.join(dataDir, "init.sql");
 const testPath = path.join(dataDir, "test.sql");
+
+const typeText = "utf-8";
 
 /**
  * Initializes the database schema by executing the SQL statements
@@ -16,7 +17,7 @@ export function initDb(db: Database.Database) {
   if (!fs.existsSync(initPath)) {
     throw new Error(`Initialization SQL file not found at path: ${initPath}`);
   }
-  const schema = fs.readFileSync(initPath, "utf-8");
+  const schema = fs.readFileSync(initPath, typeText);
 
   db.exec(schema);
 
@@ -27,7 +28,7 @@ export function initDb(db: Database.Database) {
  * Seeds the database with test data by executing the SQL statements
  */
 export function testDB(db: Database.Database) {
-  const schema = fs.readFileSync(testPath, "utf-8");
+  const schema = fs.readFileSync(testPath, typeText);
 
   db.exec(schema);
 
